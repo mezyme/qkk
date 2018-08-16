2357eb92be93597e3c636a35a9fd3394508baccc
赵悦 <zhaoyue-js@shouqiev.com>
2018-08-16 21:17:57 +0800

    UI

---
+++ src/components/Travel_Bg.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="travel-bg">
+        <img src="http://oss.squizzz.com/qkk/travel/oxpbTvqQl2VbfIXpLWKzapS_lWuw/1534135248.png">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Travel_Bg',
+  }
+</script>
+
+<style scoped :lang="scss">
+
+</style>
---
+++ src/views/Travel.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="travel-wrap">
+        <Travel_Bg/>
+    </div>
+</template>
+
+<script>
+  import Travel_Bg from '../components/Travel_Bg'
+
+  export default {
+    name: 'Travel',
+    components: Travel_Bg,
+  }
+</script>
+
+<style :lang="" scoped>
+    .travel-wrap {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+</style>
